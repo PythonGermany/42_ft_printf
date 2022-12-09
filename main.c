ffb7ecfe9292fd58c0b23ced39c0f3e68f76952d
PythonGermany <pythongermany@gmail.com>
2022-12-09 12:29:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rburgsta <rburgsta@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 15:49:56 by rburgsta          #+#    #+#             */
-/*   Updated: 2022/10/20 15:49:56 by rburgsta         ###   ########.fr       */
+/*   Updated: 2022/10/26 14:08:21 by rburgsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ int main()
 	int i = 0;
 	void *ptr = &i;
 	char *str = 0;
-	ft_printf("%i\n", ft_printf("'%%c %c'\n'%%s %s'\n'%%p %p'\n'%%d % d'\n'%%i %+i'\n'%%u %u'\n'%%x %#x'\n'%%X %#X'\n", 'x', str, ptr, 42, +42, (unsigned int)4294967290, 23141, -23141));
+	ft_printf("%i\n", ft_printf("'%%c %c'\n'%%s %s'\n'%%p %p'\n'%%d % d'\n'%%i %+i'\n'%%u %u'\n'%%x %#x'\n'%%X %#X'\n", 'x', str, ptr, 42, 42, (unsigned int)4294967290, 23141, -23141));
 	printf("%i\n", printf("'%%c %c'\n'%%s %s'\n'%%p %p'\n'%%d % d'\n'%%i %+i'\n'%%u %u'\n'%%x %#x'\n'%%X %#X'\n", 'x', str, ptr, 42, 42, (unsigned int)4294967290, 23141, -23141));
 }
