a49e7ede233b0be8dd3e854a814dc5aef1f0e845
PythonGermany <pythongermany@gmail.com>
2022-12-09 12:29:30 +0100

    Change

--- ft_printf_ptr.c
+++ ft_printf_ptr.c
@@ -6,7 +6,7 @@
 /*   By: rburgsta <rburgsta@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 16:13:20 by rburgsta          #+#    #+#             */
-/*   Updated: 2022/10/22 16:13:20 by rburgsta         ###   ########.fr       */
+/*   Updated: 2022/10/27 12:32:35 by rburgsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,8 @@ int	ft_printf_ptr(void *ptr)
 	int	len;
 
 	len = 0;
-	if (ptr)
-	{
-		ft_putstr_fd("0x", 1);
-		len += 2;
-		ft_putnbrul((unsigned long int)ptr, "0123456789abcdef", &len);
-	}
-	else
-		len += ft_printf_str("(nil)");
+	ft_putstr_fd("0x", 1);
+	len += 2;
+	ft_putnbrul((unsigned long int)ptr, "0123456789abcdef", &len);
 	return (len);
 }
